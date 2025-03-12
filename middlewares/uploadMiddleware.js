8eba22d1cf7d3830d0ceda4d830db23e68fb4a43
Gilson de Jesus <gilsonmdj@gmail.com>
2025-03-12 17:45:52 -0300

    add: create upload middleware

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 20 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype ===
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Formato de arquivo inválido. Apenas XLSX permitido"));
+    }
+  },
+});
+
+export default upload;
