d846e95a83ae14c28ed5710c624a6ff9a40d14b5
Gilson de Jesus <gilsonmdj@gmail.com>
2025-03-14 05:11:06 -0300

    refactor: improve database configuration

--- config/database.cjs
+++ config/database.cjs
@@ -1,17 +1,16 @@
-require("dotenv").config();
-
 module.exports = {
-  development: {
-    dialect: "mysql",
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
-    logging: console.log,
-    timezone: "-03:00",
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+  timezone: "-03:00",
+  dialectOptions: {
+    useUTC: true,
+    decimalNumbers: true,
   },
 };
